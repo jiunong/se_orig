d169b5ec96e2df9f6216ab01f8cbc74e4ec1a7ec
xuhong <760525825@qq.com>
2020-09-01 14:43:28 +0800

    #test

---
+++ src/main/java/com/xloud/demo/annotations/AnnotationTest.java
@@ -0,0 +1,10 @@
+package com.xloud.demo.annotations;
+
+/**
+ * TODO
+ *
+ * @author xuhong.ding
+ * @since 2020/9/1 14:41
+ */
+public class AnnotationTest {
+}
---
+++ src/main/java/com/xloud/demo/threads/ThreadTest.java
@@ -0,0 +1,10 @@
+package com.xloud.demo.threads;
+
+/**
+ * TODO
+ *
+ * @author xuhong.ding
+ * @since 2020/9/1 14:41
+ */
+public class ThreadTest {
+}
