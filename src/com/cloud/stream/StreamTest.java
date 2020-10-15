6f73b9733d15c3e873a4258d0126174f490619b5
xuhong <760525825@qq.com>
2020-10-15 09:09:46 +0800

    -stream

---
+++ src/com/cloud/stream/StreamTest.java
@@ -0,0 +1,41 @@
+package com.cloud.stream;
+
+import java.util.Arrays;
+import java.util.IllegalFormatException;
+import java.util.Optional;
+
+/**
+ * TODO
+ *
+ * @author xuhong.ding
+ * @since 2020/10/15 8:47
+ */
+public class StreamTest {
+
+
+
+    public static void main(String[] args) {
+        maxDouble("null","12","13","112");
+        //System.out.println(parseDouble(null));
+    }
+
+
+    public static void maxDouble(String... param){
+        double asDouble = Arrays.asList(param).stream().mapToDouble(StreamTest::parseDouble).max().getAsDouble();
+        System.out.println(asDouble);
+    }
+
+    public static Double parseDouble(String param) {
+        Double res = 0.0;
+        try {
+            res = Double.parseDouble(param);
+        } catch (IllegalFormatException e1) {
+            //info
+        } catch (NullPointerException e2){
+            //info
+        }finally {
+            return res;
+        }
+    }
+
+}
