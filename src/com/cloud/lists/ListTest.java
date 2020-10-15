cc8215740ecec6a36333b7e7dffc24753c68b622
xuhong <760525825@qq.com>
2020-10-15 11:01:06 +0800

    -List

--- src/com/cloud/lists/ListTest.java
+++ src/com/cloud/lists/ListTest.java
@@ -1,5 +1,7 @@
 package com.cloud.lists;
 
+import com.cloud.Model;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -13,13 +15,25 @@ public class ListTest {
 
 
     public static void main(String[] args) {
-        test1();
+        test2();
     }
 
-
-    static void  test1(){
-        List list = new ArrayList();
-        list.get(0);
+    static void test2(){
+        List<Model> list = new ArrayList();
+        Model model ;
+        for (int i = 0; i < 10; i++) {
+            model = new Model();
+            model.setKey("key"+i);
+            model.setValue("value"+i);
+            list.add(model);
+           // model =null;
+            model.setKey("key");
+            model.setValue("value");
+        }
+        for (Model model1 : list) {
+            System.out.println(model1.toString());
+        }
     }
+
 
 }
