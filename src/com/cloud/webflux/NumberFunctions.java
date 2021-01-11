7cd7f55f9bb737473d421d2c92ca98463f36b394
xuhong <760525825@qq.com>
2021-01-11 14:12:17 +0800

    - Java9 函数式编程

---
+++ src/com/cloud/webflux/NumberFunctions.java
@@ -0,0 +1,48 @@
+package com.cloud.webflux;
+
+import java.util.Optional;
+import java.util.function.Predicate;
+
+/**
+ * TODO
+ *
+ * @author xuhong.ding
+ * @since 2021/1/11 10:25
+ */
+public class NumberFunctions {
+
+
+    private Integer number;
+
+    private NumberFunctions() {
+
+    }
+
+    private static NumberFunctions numberFunctions = new NumberFunctions();
+
+    public static NumberFunctions of(Integer number) {
+        numberFunctions.number = number;
+        return numberFunctions;
+    }
+
+    public NumberFunctions add(Integer number) {
+        numberFunctions.number += number;
+        return numberFunctions;
+    }
+
+    public NumberFunctions subtraction(Integer number) {
+        numberFunctions.number -= number;
+        return numberFunctions;
+    }
+
+    public Optional<NumberFunctions> filter(Predicate<Integer> predicate) {
+        if (predicate.test(this.number)) return Optional.of(numberFunctions);
+        return Optional.of(new NumberFunctions());
+
+    }
+
+    public void operate(OperateNumberFunctions functions) {
+        functions.operate(this.number);
+    }
+
+}
