9d926df29143415314514ce3c649deaaa1dd6137
xuhong <760525825@qq.com>
2021-05-07 08:40:43 +0800

    - 枚举

---
+++ src/main/java/cloud/enums/EnumTest.java
@@ -0,0 +1,19 @@
+package cloud.enums;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * TODO
+ *
+ * @author xuhong.ding
+ * @since 2021/5/6 18:17
+ */
+public class EnumTest {
+
+    public static void main(String[] args) {
+        Arrays.stream(XmlCacheEnum.values()).map(XmlCacheEnum::getLabel).distinct().forEach(System.out::println);
+    }
+
+}
